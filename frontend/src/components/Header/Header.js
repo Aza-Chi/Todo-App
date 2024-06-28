bffd220d8305cd4183efb9f1a01c3223875ee48d
Chi <azarashibobobo@gmail.com>
2024-06-28 23:59:44 +0100

    updated Header

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -6,10 +6,10 @@ import styles from "./Header.module.css";
 export default function Header() {
   return (
     <header className={styles.header}>
-      <div className={styles.container}>
-        <div className={styles.logoContainer2}>
-        <div className={styles.logoContainer}>
           <Link to="/" className={styles.logo}> <span className={styles.title}>Todo Lists</span></Link>
+      <div className={styles.container}>
+        <div className={styles.logoContainer}>
+        <div className={styles.logoContainer2}>
         </div>
         </div>
       <MainNav />
