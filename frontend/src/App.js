dd61ad48a692329be61687c27c7b2877577e9a86
Chi <azarashibobobo@gmail.com>
2024-06-28 23:12:03 +0100

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import globalStyles from './App.module.css';
 import { Outlet } from "react-router-dom";
 import { LoginPage } from "./features/auth/LoginPage";
+import Header from "./components/Header/Header";
 import { getStatus } from './features/auth/utils';
 
 export async function authLoader() {
@@ -25,8 +26,10 @@ export function App() {
 
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
       <main>
+        <LoginPage/> 
+        {/* LoginPage component to be removed when login functionality added */}
         <Outlet />
       </main>
     </div>
---
+++ frontend/src/components/MainNav/MainNav.js
@@ -0,0 +1,70 @@
+import { NavLink, useNavigate, useRouteLoaderData } from "react-router-dom";
+import styles from "./MainNav.module.css";
+
+export default function MainNav() {
+  // https://reactrouter.com/en/main/hooks/use-route-loader-data
+  const authData = useRouteLoaderData("app");
+  const navigate = useNavigate();
+
+  async function handleClickLogOut () {
+    try {
+      console.log(`MainNav.js - Attemping Logout -`)
+      const res = await fetch(
+        `${process.env.REACT_APP_API_BASE_URL}/auth/logout`,
+        {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          credentials: "include",
+        }
+      );
+      if (!res.ok) {
+        throw new Error("Unexpected status code.");
+      }
+      console.log(`MainNav.js  Logout - res jsondata ${res.jsonData} `);
+      console.log(`MainNav.js  Logout - res  ${res} `);
+      authData.logged_in = false;
+    } catch (error) {
+      console.log(error);
+    } finally {
+      navigate('/');  // Redirect to homepage
+      //navigate(0);  // Refresh page to clear auth state and re-render
+    }
+  }
+
+  function renderNavItem(path, anchor, onClick=null) {
+    return (
+      <li className={styles.listItem}>
+        <NavLink to={path} className={styles.link} onClick={onClick}>{anchor}</NavLink>
+      </li>
+    );
+  }
+
+  return (
+    <nav className={styles.mainNav}>
+      <ul className={styles.navList}>
+        {renderNavItem("/", "Home")}
+        {/* Todo Pages! About - Contact - more*/}
+        {/* {renderNavItem("/about", "About")} */}
+
+        {/* For categories I need to add category numbers to my DB so far they're all null!*/}
+        {/* {renderNavItem("/category/tech", "Tech")}
+        {renderNavItem("/category/food", "Food")}
+        {renderNavItem("/category/misc", "Misc")} */}
+      </ul>
+      
+      {/* {authData.logged_in ?
+      <ul className={styles.navList}>
+        {renderNavItem("/account", "Account")}
+        {renderNavItem("/addresses", "Addresses")}
+        {renderNavItem("/cart", "Cart")}
+        {renderNavItem("#", "Log Out", handleClickLogOut)}
+      </ul>
+      :
+      <ul className={styles.navList}>
+        {renderNavItem("/login", "Log In")}
+        {renderNavItem("/register", "Register")}
+      </ul>
+      } */}
+    </nav>
+  );
+}
