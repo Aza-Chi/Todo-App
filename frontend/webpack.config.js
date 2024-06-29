8ab0fcf129b936f96542ce74654b78f242e8e386
Chi <azarashibobobo@gmail.com>
2024-06-29 22:52:44 +0100

    added path, dotenv-webpack

---
+++ frontend/webpack.config.js
@@ -0,0 +1,27 @@
+// webpack.config.js
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new Dotenv({
+      path: './.env', // Path to your .env file
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
