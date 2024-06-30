6ec1bf8147439f8496a4168cf7ab533e51380ccb
Chi <azarashibobobo@gmail.com>
2024-06-30 22:56:41 +0100

    updated utils

--- frontend/src/features/auth/utils.js
+++ frontend/src/features/auth/utils.js
@@ -1,15 +1,18 @@
 export async function getStatus() {
     try {
+      console.log(`getStatus() - attempting`);
       const res = await fetch(
         `${process.env.REACT_APP_API_BASE_URL}/auth/status`,
         { credentials: "include" }
       );
       if (res.ok) {
         const authData = await res.json();
+        console.log(`getStatus() - `, authData);
         return authData;
       }
       throw new Error("Unexpected status code.");
     } catch (error) {
+      
       return { logged_in: false, id: null, email_address: null, auth_method: null };
     }
   }
