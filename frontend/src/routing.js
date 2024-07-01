e4fc15c047957ab7f59843adf9196c324cad4246
Chi <azarashibobobo@gmail.com>
2024-07-01 23:55:13 +0100

    updated routing

--- frontend/src/routing.js
+++ frontend/src/routing.js
@@ -2,12 +2,11 @@ import { createBrowserRouter } from "react-router-dom";
 import { App, authLoader } from "./App";
 import ErrorPage from "./components/ErrorPage/ErrorPage";
 import { LoginPage, loginAction } from "./features/auth/LoginPage";
-import {
-  RegisterPage,
-  registerAction,
-} from "./features/auth/RegisterPage";
+import { RegisterPage, registerAction } from "./features/auth/RegisterPage";
 import AccountPage from "./components/AccountPage/AccountPage";
-// import { TodoFeed, todoFeedLoader } from "./features/products/ProductFeed";
+import TodoListFeed from "./features/TodoLists/TodoListFeed"; 
+import { todoListFeedLoader } from "./features/TodoLists/TodoListFeed"; 
+import LogoutPage from "./features/auth/LogoutPage";
 
 export const router = createBrowserRouter([
   {
@@ -23,21 +22,23 @@ export const router = createBrowserRouter([
         action: loginAction,
       },
       {
-        path: "account",
-        element: <AccountPage />,
-        // loader: ,
-      },
-      {
         path: "register",
         element: <RegisterPage />,
         action: registerAction,
       },
-    //   {
-    //     path: "",
-    //     element: <ListFeed />,
-    //     loader: listFeedLoader,
-    //   },
-      
+      {
+        path: "account",
+        element: <AccountPage />,
+      },
+      {
+        path: '/todo',
+        element: <TodoListFeed />,
+        loader: todoListFeedLoader,
+      },
+      {
+        path: '/logout',
+        element: <LogoutPage />, 
+      },
     ],
   },
 ]);
