f0319a28cc91dd5ebf4d1efa608c43db146c1278
Chi <azarashibobobo@gmail.com>
2024-07-01 23:56:05 +0100

    updated App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,41 @@
+import React, { useEffect } from 'react';
+import { Outlet, useNavigate, useLoaderData, useLocation } from 'react-router-dom';
 import globalStyles from './App.module.css';
-import { Outlet } from "react-router-dom";
-import { LoginPage } from "./features/auth/LoginPage";
-import Header from "./components/Header/Header";
-import { getStatus } from './features/auth/utils';
+import Header from './components/Header/Header';
+import { getStatus } from './features/auth/utils'; 
 
-export async function authLoader() {
-  
+export async function authLoader() {  
 return await getStatus();
-
 }
 
 export function App() {
-  
+  const authData = useLoaderData();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    // Check authData.logged_in after navigating to the root route or after redirect
+    getStatus();
+    if (location.pathname === '/' && authData.logged_in) {
+      console.log(`App.js - authData: ${authData.id}`);
+      navigate('/todo'); // Redirect to /todo if logged in and navigating to /
+    } else if (location.pathname === '/' && !authData.logged_in) {
+      console.log(`App.js - authData: ${authData.id}`);
+      navigate('/login'); // Redirect to /login if not logged in and navigating to /
+    }
+  }, [authData, navigate, location.pathname]);
+
+  return (
+    <div className={globalStyles.App}>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
+// import { LoginPage } from "./features/auth/LoginPage";
 
   // if (!authData.logged_in) {
   //   return (
@@ -22,16 +46,3 @@ export function App() {
 
   // );
   // }
-
-
-  return (
-    <div className="App">
-      <Header />
-      <main>
-        {/* <LoginPage/>  */}
-        {/* LoginPage component to be removed when login functionality added */}
-        <Outlet />
-      </main>
-    </div>
-  );
-}
