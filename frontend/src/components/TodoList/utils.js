e1d8a49f08bce708046472fcf69c3c4e4d1cfebb
Chi <azarashibobobo@gmail.com>
2024-07-02 18:45:23 +0100

    updated utils

--- frontend/src/components/TodoList/utils.js
+++ frontend/src/components/TodoList/utils.js
@@ -1,5 +1,7 @@
 export const fetchTodos = async (listId, setTodos, setError) => {
     try {
+      const a = `${process.env.REACT_APP_API_BASE_URL}/todos/list/${listId}`;
+      console.log(`utils.js - fetching Todos with listId:`, a);
       const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/todos/list/${listId}`);
       if (!response.ok) {
         if (response.status === 404) {
@@ -17,3 +19,48 @@ export const fetchTodos = async (listId, setTodos, setError) => {
       setError(error);
     }
   };
+
+// 
+export async function updateListName(listId, newName) {
+  try {
+    const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/lists/list/${listId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        list_name: newName,
+      }),
+    });
+    if (!response.ok) {
+      throw new Error('Failed to update list name');
+    }
+    const updatedList = await response.json();
+    return updatedList;
+  } catch (error) {
+    throw new Error(`Error updating list name: ${error.message}`);
+  }
+}
+
+
+
+export async function updateOrderNum(listId, newOrderNum) {
+  try {
+    const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/lists/list/${listId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        order_num: newOrderNum,
+      }),
+    });
+    if (!response.ok) {
+      throw new Error('Failed to update order number');
+    }
+    const updatedList = await response.json();
+    return updatedList;
+  } catch (error) {
+    throw new Error(`Error updating order number: ${error.message}`);
+  }
+}
